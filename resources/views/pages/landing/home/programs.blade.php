7650ab96976a97c35ccd9592a4e7159460a338ec
berrylradianh <berrylhamesha@gmail.com>
2025-03-04 22:31:30 +0700

    feat: resource section

---
+++ resources/views/pages/landing/home/programs.blade.php
@@ -0,0 +1,5 @@
+<section class="counter__area">
+    <div class="container">
+        <img src="{{asset('assets/img/resource.png')}}" alt="Counter" style="width: 100%; height: auto;">
+    </div>
+</section>
