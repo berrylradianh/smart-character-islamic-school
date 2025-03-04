d1b7843d89a0e47d2eaaac3daf7b9671e6917909
berrylradianh <berrylhamesha@gmail.com>
2025-03-05 01:27:03 +0700

    fix: title variable on app

---
+++ app/Http/Controllers/Landing/LandingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Landing;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LandingController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Beranda',
+        ];
+
+        return view('pages.landing.home.index', $data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Landing\LandingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.landing.home.index');
-});
+Route::get('/', [LandingController::class, 'index'])->name('landing.home');
