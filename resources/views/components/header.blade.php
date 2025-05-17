649cf48f7b68c2fc1e3ca0650dc7b40835146bbe
Berryl Radian Hamesha <91478990+berrylradianh@users.noreply.github.com>
2025-05-18 00:41:39 +0700

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -159,7 +159,7 @@
                                     style="display: inline-flex; align-items: center; padding: 8px 15px; background-color: #E47804; color: #fff; text-decoration: none; border-radius: 4px; transition: background-color 0.3s ease-in-out;"
                                     onmouseover="this.style.backgroundColor='#FF9800';"
                                     onmouseout="this.style.backgroundColor='#E47804';">
-                                    Login Admin
+                                    Login 
                                 </a>
                                 <a href="{{ route('auth.login') }}" class="header-btn"
                                     style="display: inline-flex; align-items: center; padding: 8px 15px; background-color: #1E88E5; color: #fff; text-decoration: none; border-radius: 4px; transition: background-color 0.3s ease-in-out;"
